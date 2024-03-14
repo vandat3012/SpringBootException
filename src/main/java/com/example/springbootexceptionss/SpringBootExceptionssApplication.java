de351127bc50e1b45b70b799bd5df6d40d95d7fa
vandat3012 <luongvandat301220@gmail.com>
2024-03-14 16:37:24 +0700

    update

---
+++ src/main/java/com/example/springbootexceptionss/SpringBootExceptionssApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootexceptionss;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootExceptionssApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootExceptionssApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springbootexceptionss/SpringBootExceptionssApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootexceptionss;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootExceptionssApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
